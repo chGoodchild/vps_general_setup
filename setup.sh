4854f98c27500d1509297fd3e09af10aa05df901
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-08-13 19:36:40 +0200

    Setup git

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 
 
 sudo apt update -y
-sudo apt install -y emacs screen
+sudo apt install -y emacs screen git
 
-
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
