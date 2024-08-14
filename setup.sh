41c4ed3ca06b5b8231eece675ba9bce88db8e2b1
Your Name <you@example.com>
2024-08-14 07:33:32 +0000

    Non interactive emacs install

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 
 
 sudo apt update -y
-sudo apt install -y emacs screen git
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y emacs
+sudo apt install -y screen git
 
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
