4b1eebae99f23282ec4569d8502655bdc23181fd
Your Name <you@example.com>
2024-08-14 07:37:55 +0000

    Log into github

--- setup.sh
+++ setup.sh
@@ -2,7 +2,12 @@
 
 sudo apt update -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y emacs
-sudo apt install -y screen git
+sudo apt install -y screen git gh
 
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
+
+gh auth login
+gh auth status
+
+
